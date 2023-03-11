fdf678c5f55139cb1106d979bddffef91cbb21dd
Besliu-Madalina <104641549+Besliu-Madalina@users.noreply.github.com>
2023-03-11 22:32:03 +0200

    Add files via upload

---
+++ js/lightbox.js
@@ -0,0 +1,151 @@
+(function(root, factory) {
+
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        root.SimpleLightbox = factory();
+    }
+
+}(this, function() {
+
+    function assign(target) {
+
+        for (var i = 1; i < arguments.length; i++) {
+
+            var obj = arguments[i];
+
+            if (obj) {
+                for (var key in obj) {
+                    obj.hasOwnProperty(key) && (target[key] = obj[key]);
+                }
+            }
+
+        }
+
+        return target;
+
+    }
+
+    function addClass(element, className) {
+
+        if (element && className) {
+            element.className += ' ' + className;
+        }
+
+    }
+
+    function removeClass(element, className) {
+
+        if (element && className) {
+            element.className = element.className.replace(
+                new RegExp('(\\s|^)' + className + '(\\s|$)'), ' '
+            ).trim();
+        }
+
+    }
+
+    function parseHtml(html) {
+
+        var div = document.createElement('div');
+        div.innerHTML = html.trim();
+
+        return div.childNodes[0];
+
+    }
+
+    function matches(el, selector) {
+
+        return (el.matches || el.matchesSelector || el.msMatchesSelector).call(el, selector);
+
+    }
+
+    function getWindowHeight() {
+
+        return 'innerHeight' in window
+            ? window.innerHeight
+            : document.documentElement.offsetHeight;
+
+    }
+
+    function SimpleLightbox(options) {
+
+        this.init.apply(this, arguments);
+
+    }
+
+    SimpleLightbox.defaults = {
+
+        // add custom classes to lightbox elements
+        elementClass: '',
+        elementLoadingClass: 'slbLoading',
+        htmlClass: 'slbActive',
+        closeBtnClass: '',
+        nextBtnClass: '',
+        prevBtnClass: '',
+        loadingTextClass: '',
+
+        // customize / localize controls captions
+        closeBtnCaption: 'Close',
+        nextBtnCaption: 'Next',
+        prevBtnCaption: 'Previous',
+        loadingCaption: 'Loading...',
+
+        bindToItems: true, // set click event handler to trigger lightbox on provided $items
+        closeOnOverlayClick: true,
+        closeOnEscapeKey: true,
+        nextOnImageClick: true,
+        showCaptions: true,
+
+        captionAttribute: 'title', // choose data source for library to glean image caption from
+        urlAttribute: 'href', // where to expect large image
+
+        startAt: 0, // start gallery at custom index
+        loadingTimeout: 100, // time after loading element will appear
+
+        appendTarget: 'body', // append elsewhere if needed
+
+        beforeSetContent: null, // convenient hooks for extending library behavoiur
+        beforeClose: null,
+        afterClose: null,
+        beforeDestroy: null,
+        afterDestroy: null,
+
+        videoRegex: new RegExp(/youtube.com|vimeo.com/) // regex which tests load url for iframe content
+
+    };
+
+    assign(SimpleLightbox.prototype, {
+
+        init: function(options) {
+
+            options = this.options = assign({}, SimpleLightbox.defaults, options);
+
+            var self = this;
+            var elements;
+
+            if (options.$items) {
+                elements = options.$items.get();
+            }
+
+            if (options.elements) {
+                elements = [].slice.call(
+                    typeof options.elements === 'string'
+                        ? document.querySelectorAll(options.elements)
+                        : options.elements
+                );
+            }
+
+            this.eventRegistry = {lightbox: [], thumbnails: []};
+            this.items = [];
+            this.captions = [];
+
+            if (elements) {
+
+                elements.forEach(function(element, index) {
+
+                    self.items.push(element.getAttribute(options.urlAttribute));
+                    self.captions.push(element.getAttribute(options.captionAttribute));
+
+                    i
